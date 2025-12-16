bcda70b2c82a8d8bd7b403c6bb285765d73c3e1b
Jesus Arias Fisteus <jfisteus@gmail.com>
2025-12-16 12:11:08 +0100

    Ability to "forget" entries from the sent log
    This way, an email can be sent again to selected students.

--- mailst/sent_log.py
+++ mailst/sent_log.py
@@ -1,9 +1,18 @@
 import datetime
 import pathlib
+import enum
 
 from . import address
 
 LOG_FILENAME = ".mailst-sentlog.csv"
+
+
+class Action(enum.Enum):
+    SEND = "send"
+    FORGET = "forget"
+
+    def __str__(self) -> str:
+        return self.value
 
 
 class SentLog:
@@ -18,24 +27,34 @@ class SentLog:
 
     def add(self, address: address.Address, timestamp: datetime.datetime) -> None:
         self.sent_to[address.email] = timestamp
-        self._append_to_file(address, timestamp)
+        self._append_to_file(address, Action.SEND, timestamp)
+
+    def forget(self, address: address.Address) -> None:
+        if address.email in self.sent_to:
+            del self.sent_to[address.email]
+            self._append_to_file(address, Action.FORGET, datetime.datetime.now())
 
     def _load_from_file(self) -> None:
         path = pathlib.Path(LOG_FILENAME)
         if path.exists():
             with open(LOG_FILENAME, "r") as f:
                 for line in f:
-                    email, timestamp = line.strip().split(",")
-                    self.sent_to[email] = datetime.datetime.fromisoformat(timestamp)
+                    email, action_str, timestamp = line.strip().split(",")
+                    action = Action(action_str)
+                    if action == Action.SEND:
+                        self.sent_to[email] = datetime.datetime.fromisoformat(timestamp)
+                    elif action == Action.FORGET:
+                        if email in self.sent_to:
+                            del self.sent_to[email]
             print(f"Loaded {len(self.sent_to)} sent email records from the log file.")
         else:
             print("No log file found. Starting with an empty log.")
 
     def _append_to_file(
-        self, address: address.Address, timestamp: datetime.datetime
+        self, address: address.Address, action, timestamp: datetime.datetime
     ) -> None:
         with open(LOG_FILENAME, "a") as f:
-            f.write(f"{address.email},{timestamp.isoformat()}\n")
+            f.write(f"{address.email},{action},{timestamp.isoformat()}\n")
 
 
 log = SentLog()
